d2285bd7800cef7088f952d365a9ba294b8a0d4c
Dhruv <dhruvgoplani00@gmail.com>
2024-04-04 19:09:01 +0530

    Admin screens created and admin class created

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:judge_assist_app/features/presentation/providers/event_provider.dart';
+import 'package:judge_assist_app/features/presentation/screens/create_team.dart';
+import 'package:judge_assist_app/features/presentation/screens/event_screen.dart';
 import 'package:judge_assist_app/features/presentation/screens/home_screen.dart';
 import 'package:provider/provider.dart';
+
+import 'features/presentation/screens/AdminHomeScreen.dart';
+
+
 
 void main() {
   runApp(ChangeNotifierProvider(
@@ -11,13 +17,18 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color(0xFF0F0F1F),
+        appBarTheme: AppBarTheme(
+          color: Color(0xFF1D1D2F),
+        )
+      ),
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      home: HomeScreen(),
+      title: 'Judge Assist',
+      home: AdminHomeScreen(),
     );
   }
 }
